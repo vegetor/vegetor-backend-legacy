07b56531c7d44b936bef5a3dfb7fbaeb93eb7dea
juneyoungoh <xzawed85>
2020-02-10 20:00:33 +0900

    checkout login branch

--- sequelize.ts
+++ sequelize.ts
@@ -7,7 +7,5 @@ export const sequelize = new Sequelize({
   username: 'vegetor',
   password: 'vegetor',
   port: 3306,
-  // host: '127.0.0.1',
-  // password: 'xzawed85',
   models: [__dirname + '/models_v2']
 })
